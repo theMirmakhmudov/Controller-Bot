3017da329b6859ab88f9813c11ba53a2dabf385c
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-03-29 08:01:04 +0500

    #Commit

--- handlers.py
+++ handlers.py
@@ -1,8 +1,12 @@
 from aiogram import types
-from config import Admin
 from aiogram import Bot
-from aiogram.types import ChatPermissions
-from datetime import timedelta, datetime
+from aiogram.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboardButton
+import time
+from config import Admin, TOKEN
+
+add_group = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Guruhga qo'shish", url="http://t.me/PDPYordamchi_bot?startgroup=start")]
+])
 
 
 async def start_up(bot: Bot) -> None:
@@ -13,49 +17,121 @@ async def on_shutdown(bot: Bot) -> None:
     await bot.send_message(Admin, "Bot ishdan to'xtadi ❌")
 
 
-async def new_chat_member_answer(message: types.Message):
+async def cmd_start(message: types.Message, bot: Bot):
+    bot_user = await bot.get_chat_member(message.chat.id, TOKEN.split(":")[0])
+
+    await message.answer(f"""<b>
+Assalomu Alaykum {message.from_user.mention_html()} ❗️
+        
+👮🏻‍♂️ Men guruhingizga qo'shilgan odamlar bilan salomlashaman va guruhni boshqarishda yordam beraman ✅
+
+❗️Guruhingizda muammosiz ishlashim uchun adminlik xuquqini bering!
+
+🤖 {bot_user.user.mention_html()}
+</b>""", reply_markup=add_group)
+
+
+async def new_chat_member_answer(message: types.Message, bot: Bot):
     for new_chat_member in message.new_chat_members:
-        await message.answer_photo(
+        mes = await message.answer_photo(
             "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2023/07/computer-coding.jpg",
-            f"Assalomu Aleykum, {new_chat_member.mention_html()}\nGuruhimizga Xush kelibsiz!")
+            f"<b>Assalomu Aleykum, {new_chat_member.mention_html()}\nGuruhimizga Xush kelibsiz 🎉</b>")
 
-        await message.delete()
+        time.sleep(20)
+        await bot.delete_message(chat_id=message.chat.id, message_id=mes.message_id)
 
 
-async def left_chat_member_answer(message: types.Message):
-    await message.answer_photo(
+async def left_chat_member_answer(message: types.Message, bot: Bot):
+    mes = await message.answer_photo(
         "https://image.shutterstock.com/image-vector/3d-isometric-flat-vector-conceptual-260nw-2373700103.jpg",
-        f"Xayir, {message.left_chat_member.mention_html()}")
+        f"<b>Xayir, {message.left_chat_member.mention_html()}</b>")
 
-    await message.delete()
-
-
-async def mute_member_answer(message: types.Message):
-    user_id = message.reply_to_message.from_user.id
-    permissions = ChatPermissions(can_send_messages=False)
-    await message.chat.restrict(user_id, permissions)
-    await message.answer_photo(
-        "https://media.istockphoto.com/id/1305893663/vector/silent-sound-off-icon-vector-for-your-web-design-logo-ui-illustration.jpg?s=612x612&w=0&k=20&c=czrINWt2weKC3fLHU3KqI2eZBFdwhOuuCZxS5JNGpSU=",
-        f"{message.reply_to_message.from_user.mention_html()} yozishdan maxrum qilindi ❗️")
+    time.sleep(20)
+    await bot.delete_message(chat_id=message.chat.id, message_id=mes.message_id)
 
 
-async def unmute_member_answer(message: types.Message):
-    user_id = message.reply_to_message.from_user.id
-    permissions = ChatPermissions(can_send_messages=True)
-    await message.chat.restrict(user_id, permissions)
-    await message.answer_photo(
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlPQKfIi9UUnwcNasWrtRXuZGvu1pUq1_XRw&usqp=CAU",
-        f"✅{message.reply_to_message.from_user.mention_html()} endi yoza olasiz ❗️️")
+async def mute_member_answer(message: types.Message, bot: Bot):
+    try:
+        user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+        if user_status.status == "creator" or user_status == "administrator":
+            user_id = message.reply_to_message.from_user.id
+            permissions = ChatPermissions(can_send_messages=False)
+            await message.chat.restrict(user_id, permissions)
+            mes1 = await message.answer_photo(
+                "https://businesswired.files.wordpress.com/2014/05/twitter-mute-lo-res.jpg",
+                f"<b>{message.reply_to_message.from_user.mention_html()} yozishdan maxrum qilindi ❗️</b>")
+
+            time.sleep(20)
+            await bot.delete_message(chat_id=message.chat.id, message_id=mes1.message_id)
+        else:
+            mes2 = await message.answer("<b>You're not Admin ❌</b>")
+            await bot.delete_message(chat_id=message.chat.id, message_id=mes2.message_id)
+    except:
+        await message.answer("<b>Siz Adminni mute qila olmaysiz ❌</b>")
 
 
-async def ban_member_answer(message: types.Message):
-    user_id = message.reply_to_message.from_user.id
-    await message.chat.ban_sender_chat(user_id)
-    await message.answer(f"{message.reply_to_message.from_user.mention_html()} guruhdan chetlashtirildi ❗️")
+async def unmute_member_answer(message: types.Message, bot: Bot):
+    user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+    if user_status.status == "creator" or user_status == "administrator":
+        user_id = message.reply_to_message.from_user.id
+        permissions = ChatPermissions(can_send_messages=True)
+        await message.chat.restrict(user_id, permissions)
+        mes1 = await message.answer_photo(
+            "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg",
+            f"<b>✅{message.reply_to_message.from_user.mention_html()} endi yoza olasiz ❗️️</b>")
+
+        time.sleep(20)
+        await bot.delete_message(chat_id=message.chat.id, message_id=mes1.message_id)
+    else:
+        mes2 = await message.answer("<b>You're not Admin ❌</b>")
+        await bot.delete_message(chat_id=message.chat.id, message_id=mes2.message_id)
 
 
-async def unban_member_answer(message: types.Message):
-    user_id = message.reply_to_message.from_user.id
-    await message.chat.unban_sender_chat(user_id)
-    await message.answer(
-        f"{message.reply_to_message.from_user.mention_html()} guruhga qayta qo'shilish uchun ruxsat berildi ❗️")
+async def deleted_messages(message: types.Message, bot: Bot):
+    user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+    if user_status.status == "creator" or user_status == "administrator":
+        mes = message.reply_to_message.message_id
+        await bot.delete_message(chat_id=message.chat.id, message_id=mes, request_timeout=15)
+        await message.delete()
+    else:
+        mes = await message.answer("<b>You're not Admin ❌</b>")
+        await bot.delete_message(chat_id=message.chat.id, message_id=mes.message_id)
+
+
+async def get_channel_id(message: types.Message):
+    await message.answer(f"<b>This chat's ID is: </b><code>{message.chat.id}</code>")
+
+
+async def ban_member_answer(message: types.Message, bot: Bot):
+    try:
+        user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+        if user_status.status == "creator" or user_status == "administrator":
+            user_id = message.reply_to_message.from_user.id
+            await message.chat.ban_sender_chat(user_id)
+            mes = await message.answer(
+                f"<b>{message.reply_to_message.from_user.mention_html()} guruhdan chetlashtirildi ❗️</b>")
+            time.sleep(20)
+            await bot.delete_message(chat_id=message.chat.id, message_id=mes.message_id)
+
+        else:
+            mes2 = await message.answer("You're not Admin ❌")
+            await bot.delete_message(chat_id=message.chat.id, message_id=mes2.message_id)
+
+    except:
+        await message.answer("<b>Siz adminga ban bera olmaysiz ❌</b>")
+
+
+async def unban_member_answer(message: types.Message, bot: Bot):
+    user_status = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+    if user_status.status == "creator" or user_status == "administrator":
+        user_id = message.reply_to_message.from_user.id
+        await message.chat.unban_sender_chat(user_id)
+        mes = await message.answer(
+            f"<b>{message.reply_to_message.from_user.mention_html()} guruhga qayta qo'shilish uchun ruxsat berildi ❗️</b>")
+
+        time.sleep(20)
+        await bot.delete_message(chat_id=message.chat.id, message_id=mes.message_id)
+
+    else:
+        mes2 = await message.answer("You're not Admin ❌")
+        await bot.delete_message(chat_id=message.chat_id, message_id=mes2.message_id)
--- main.py
+++ main.py
@@ -3,31 +3,36 @@ import logging
 from aiogram import Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.filters import Command, and_f
-from aiogram.types import Message
-from config import TOKEN, Channel
+from config import TOKEN
 import handlers
 from aiogram import Bot
 
 dp = Dispatcher()
 
 
-@dp.message(Command("start"))
-async def start(message: Message):
-    await message.answer("Salom !")
-
-
 async def main() -> None:
     dp.startup.register(handlers.start_up)
     dp.shutdown.register(handlers.on_shutdown)
-    dp.message.register(handlers.new_chat_member_answer, and_f(F.chat.id == Channel, F.new_chat_members))
-    dp.message.register(handlers.left_chat_member_answer, and_f(F.chat.id == Channel, F.left_chat_member))
-    dp.message.register(handlers.mute_member_answer, and_f(F.chat.type == "supergroup", and_f(F.text == "/mute", F.reply_to_message)))
-    dp.message.register(handlers.unmute_member_answer, and_f(F.chat.type == "supergroup", and_f(F.text == "/unmute", F.reply_to_message)))
-    dp.message.register(handlers.ban_member_answer, and_f(F.chat.type == "supergroup", and_f(F.text == "/ban", F.reply_to_message)))
-    dp.message.register(handlers.unban_member_answer, and_f(F.chat.type == "supergroup", and_f(F.text == "/unban", F.reply_to_message)))
+    dp.message.register(handlers.cmd_start, Command("start"))
+    dp.message.register(handlers.new_chat_member_answer,
+                        and_f(F.chat.type == "group" or F.chat.type == "supergroup", F.new_chat_members))
+    dp.message.register(handlers.left_chat_member_answer,
+                        and_f(F.chat.type == "group" or F.chat.type == "supergroup", F.left_chat_member))
+    dp.message.register(handlers.mute_member_answer,
+                        and_f(F.chat.type == "supergroup", and_f(F.text == "/mute", F.reply_to_message)))
+    dp.message.register(handlers.unmute_member_answer,
+                        and_f(F.chat.type == "supergroup", and_f(F.text == "/unmute", F.reply_to_message)))
+    dp.message.register(handlers.ban_member_answer,
+                        and_f(F.chat.type == "supergroup", and_f(F.text == "/ban", F.reply_to_message)))
+    dp.message.register(handlers.unban_member_answer,
+                        and_f(F.chat.type == "supergroup", and_f(F.text == "/unban", F.reply_to_message)))
+    dp.message.register(handlers.deleted_messages,
+                        and_f(F.chat.type == "supergroup", and_f(F.text == "/delete", F.reply_to_message)))
+    dp.message.register(handlers.get_channel_id, and_f(F.text == "/id"))
 
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, polling_timeout=1)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
